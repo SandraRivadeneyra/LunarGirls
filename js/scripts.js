7eda6de3b2d04a9462f1222e36beaefce9b39d4c
SandraRivadeneyra <sandymoon0211@gmail.com>
2024-06-28 22:00:18 -0600

    Primer proyecto

---
+++ js/scripts.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // Validación de formularios
+    const forms = document.querySelectorAll('.needs-validation');
+    Array.prototype.slice.call(forms).forEach(function (form) {
+        form.addEventListener('submit', function (event) {
+            if (!form.checkValidity()) {
+                event.preventDefault();
+                event.stopPropagation();
+            }
+            form.classList.add('was-validated');
+        }, false);
+    });
+
+    // Smooth Scroll
+    const scrollLinks = document.querySelectorAll('.scroll-link');
+    scrollLinks.forEach(link => {
+        link.addEventListener('click', function (e) {
+            e.preventDefault();
+            const target = document.querySelector(this.getAttribute('href'));
+            target.scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
+});
